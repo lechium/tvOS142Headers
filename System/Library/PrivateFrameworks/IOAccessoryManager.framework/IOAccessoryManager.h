c7696f6d760e4822f61b9f2c2adcd18749700fda
Kevin Bradley <kevin.w.bradley@me.com>
2020-11-10 22:37:26 -0700

    initial commit

---
+++ System/Library/PrivateFrameworks/IOAccessoryManager.framework/IOAccessoryManager.h
@@ -0,0 +1,4 @@
+#import <IOAccessoryManager/HalogenTypeC.h>
+#import <IOAccessoryManager/HalogenAudio.h>
+#import <IOAccessoryManager/HalogenLdcmCalc.h>
+#import <IOAccessoryManager/HalogenMeasurement.h>
